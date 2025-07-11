7ecafa6dc72cbc4e5912afa8d8c560797f6626fa
aman2606 <aman8987668356@gmail.com>
2025-07-11 21:15:14 +0530

    Update API base URL to production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance
 export const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://urjaa-backend.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
